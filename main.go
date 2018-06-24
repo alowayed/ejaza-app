66139d27eb72529347ad34ed1b98b80a9493ceeb
Yousef Alowayed <alowayed@mit.edu>
2018-06-24 15:13:56 -0400

    Implement add to blockchain

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/julienschmidt/httprouter"
+	"strconv"
+	"github.com/go-redis/redis"
+	"math/rand"
+	"encoding/json"
+	"time"
+	"io/ioutil"
+	"bytes"
 )
+
+var client *redis.Client
+
+func setupDB() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
 
 // ---------- Types
 
@@ -20,12 +36,23 @@ type Cert struct {
 	Iemail    string
 	Nonce     string
 	Confirmed bool
+	BlockchainId string
+}
+
+type TerionResp struct {
+	BlockchainId string `json:"id"`
+	AccountId int `json:"accountId"`
+	DatastoreId int `json:"datastoreId"`
+	Status string `json:"status"`
+	Data Cert
+	Json string `json:"json"`
+	Sha256 string `json:"sha256"`
+	Timestamp int `json:"timestamp"`
 }
 
 // ---------- Global Variables
 
 var tpl *template.Template
-var Certs []Cert
 
 // Push the given resource to the client.
 // func push(w http.ResponseWriter, resource string) {
@@ -41,11 +68,13 @@ var Certs []Cert
 // ---------- Routes
 
 func GetIndex(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	preHandler(&w, req, ps)
 	tpl.ExecuteTemplate(w, "index", nil)
 }
 
 func GetCert(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	tpl.ExecuteTemplate(w, "index", nil)
+	preHandler(&w, req, ps)
+	tpl.ExecuteTemplate(w, "cert", nil)
 }
 
 func GetCertSubmit(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
@@ -58,7 +87,8 @@ func PostCertSubmit(w http.ResponseWriter, req *http.Request, ps httprouter.Para
 		log.Fatalln(err)
 	}
 
-	id := len(Certs)
+	id := generateId()
+
 	data := req.Form["data"][0]
 	sEmail := req.Form["semail"][0]
 	iEmail := req.Form["iemail"][0]
@@ -69,16 +99,38 @@ func PostCertSubmit(w http.ResponseWriter, req *http.Request, ps httprouter.Para
 		Semail:    sEmail,
 		Iemail:    iEmail,
 		Nonce:     "001",
-		Confirmed: false,
 	}
 
-	Certs = append(Certs, cert)
+	strId := strconv.Itoa(cert.Id)
 
-	// TODO: Err handle if cert cannor be added to DB
+	certJson, err := json.Marshal(cert)
+	if err != nil {
+		panic (err)
+	}
+
+	err = client.Set(strId, certJson, 0).Err()
+	if err != nil {
+		fmt.Println("getting cert by id: ", strId)
+		url := fmt.Sprintf("/cert/id/%v", id)
+		http.Redirect(w, req, url, http.StatusSeeOther)
+
+	}
+	// TODO: Err handle if cert cannot be added to DB
+
+	saveCertToBlockchain(cert)
 
 	// Redirect to this certificate's page (should be pending confirmation)
 	url := fmt.Sprintf("/cert/id/%v", id)
 	http.Redirect(w, req, url, http.StatusSeeOther)
+}
+
+func generateId() int {
+
+	id := rand.Int()
+
+	// TODO: check if ID in database
+
+	return id
 }
 
 func GetCertSubmitted(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
@@ -86,11 +138,34 @@ func GetCertSubmitted(w http.ResponseWriter, req *http.Request, ps httprouter.Pa
 }
 
 func GetCertById(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	preHandler(&w, req, ps)
 
-	// TODO: check if id is in database
+	id := ps.ByName("id")
+	certId, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println("certId from param: ", certId)
+	}
 
-	io.WriteString(w, fmt.Sprintf("%v", Certs))
-	// fmt.Fprint(w, Certs)
+	itemJson := client.Get(id).Val()
+	if itemJson == "" {
+		fmt.Println("Cert does not exist")
+		tpl.ExecuteTemplate(w, "cert", id)
+		return
+	}
+
+	item := &Cert{}
+	unmarshalErr := json.Unmarshal([]byte(itemJson), item)
+	if unmarshalErr != nil {
+		fmt.Println(unmarshalErr)
+	}
+
+	//fmt.Println("item", item.String())
+	//if item.Err() != redis.Nil {
+	//	fmt.Println("cannot find cert with id: ", certId)
+	//	// TODO
+	//}
+
+	tpl.ExecuteTemplate(w, "certById", item)
 }
 
 func GetCertConfirmById(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
@@ -101,7 +176,72 @@ func GetCertConfirmById(w http.ResponseWriter, req *http.Request, ps httprouter.
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+
+	setupDB()
 }
+
+func preHandler(w *http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	enableCors(w)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func saveCertToBlockchain(cert Cert) {
+
+	url := "https://api.tierion.com/v1/records"
+
+	reqCert := struct {
+		DataStoreId int `json:"datastoreId"`
+		Id        int	`json:"id"`
+		Data      string`json:"data"`
+		Semail    string`json:"semail"`
+		Iemail    string`json:"iemail"`
+	}{
+		DataStoreId: 7485,
+		Id: cert.Id,
+		Data: cert.Data,
+		Semail: cert.Semail,
+		Iemail: cert.Iemail,
+	}
+
+	jsonString, err := json.Marshal(reqCert)
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("X-Username", "ejazaco@gmail.com")
+	req.Header.Set("X-Api-Key", "hALcxG45opSVjr2e/qpjOn3260jiKVQEe3eUc/hxAF4=")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	fmt.Println("resp status: ", res.Status)
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var terionResp TerionResp
+	unmarshalErr := json.Unmarshal(body, &terionResp)
+	if unmarshalErr != nil {
+		fmt.Println("Failed to parse response from Terion: ", unmarshalErr)
+	}
+
+	fmt.Println("body: ", terionResp)
+}
+
 
 func main() {
 
@@ -111,10 +251,12 @@ func main() {
 
 	mux.GET("/", GetIndex)
 	mux.GET("/cert", GetCert)
+	mux.POST("/cert", GetCert)
 	mux.GET("/cert/submit", GetCertSubmit)
 	mux.POST("/cert/submit", PostCertSubmit)
 	mux.GET("/cert/submitted", GetCertSubmitted)
 	mux.GET("/cert/id/:id", GetCertById)
+	mux.POST("/cert/id/:id", GetCertById)
 	mux.GET("/cert/confirm/:id", GetCertConfirmById)
 
 	port := os.Getenv("PORT")
